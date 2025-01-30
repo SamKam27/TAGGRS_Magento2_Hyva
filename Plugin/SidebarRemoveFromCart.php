446117a68cc2ce0eb00bc179803c223ed40a70d7
Webwijs <development@webwijs.nu>
2025-01-30 10:44:11 +0000

    initial commit

---
+++ Plugin/SidebarRemoveFromCart.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Hyva\TaggrsDataLayer\Plugin;
+
+use Magento\Checkout\Controller\Sidebar\RemoveItem;
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\ObjectManager;
+use Magento\Quote\Api\CartItemRepositoryInterface;
+use Magento\Quote\Api\Data\CartItemInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Psr\Log\LoggerInterface;
+use Taggrs\DataLayer\Api\DataLayerInterface;
+use Taggrs\DataLayer\Helper\ProductViewDataHelper;
+use Taggrs\DataLayer\Helper\UserDataHelper;
+use Magento\Checkout\Model\Session as CheckoutSession;
+
+class SidebarRemoveFromCart implements DataLayerInterface
+{
+
+    private Session $session;
+
+    private UserDataHelper $userDataHelper;
+
+    private ProductViewDataHelper $productDataHelper;
+
+    private CartItemRepositoryInterface $cartItemRepository;
+
+    private CheckoutSession $checkoutSession;
+
+    private StoreManagerInterface $storeManager;
+
+    private ?CartItemInterface $removedItem = null;
+
+
+    public function __construct(
+        Session                     $session,
+        UserDataHelper              $userDataHelper,
+        ProductViewDataHelper       $productDataHelper,
+        CartItemRepositoryInterface $cartItemRepository,
+        CheckoutSession             $checkoutSession,
+        StoreManagerInterface       $storeManager
+    ) {
+        $this->session = $session;
+        $this->userDataHelper = $userDataHelper;
+        $this->productDataHelper = $productDataHelper;
+        $this->cartItemRepository = $cartItemRepository;
+        $this->checkoutSession = $checkoutSession;
+        $this->storeManager = $storeManager;
+    }
+
+    public function beforeExecute(RemoveItem $subject)
+    {
+        $request = $subject->getRequest();
+        $quoteItemId = $request->getParam('item_id');
+        ObjectManager::getInstance()->get(LoggerInterface::class)->critical('REMOVED ITEM FROM SIDEBAR ' . $quoteItemId);
+
+        if ($quoteItemId !== null) {
+            foreach ($this->checkoutSession->getQuote()->getItems() as $quoteItem) {
+                if ($quoteItemId == $quoteItem->getItemId()) {
+                    $this->removedItem = $quoteItem;
+                    break;
+                }
+            }
+        }
+
+        if ($this->removedItem !== null) {
+            $this->session->setDataLayer($this->getDataLayer());
+        }
+    }
+
+    public function getEvent(): string
+    {
+        return 'remove_from_cart';
+    }
+
+    public function getEcommerce(): array
+    {
+        $ecommerce = ['currency' => $this->storeManager->getStore()->getCurrentCurrency()->getCode()];
+
+        if ($this->removedItem !== null) {
+
+            $ecommerce['value'] = floatval($this->removedItem->getPriceInclTax()) * $this->removedItem->getQty();
+
+            $item = [
+                'item_id' => $this->removedItem->getProduct()->getId(),
+                'item_name' => $this->removedItem->getProduct()->getName(),
+                'quantity' => $this->removedItem->getQty(),
+                'price' => floatval($this->removedItem->getPriceInclTax()),
+            ];
+
+            $item = array_merge($item, $this->productDataHelper->getCategoryNamesByProduct($this->removedItem->getProduct()));
+
+            $ecommerce['items'] = [$item];
+
+            $ecommerce['user_data'] = $this->getUserData();
+        }
+
+        return $ecommerce;
+    }
+
+    public function getUserData(): array
+    {
+        return $this->userDataHelper->getUserData();
+    }
+
+    public function getDataLayer(): array
+    {
+        return [
+            'event' => $this->getEvent(),
+            'ecommerce' => $this->getEcommerce(),
+
+        ];
+    }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# Taggrs GTM DataLayer for Magento 2
+This extension collect eccommerce data from Magento 2 and pushes it to the Google Tag Manager DataLayer.
+
+## Installation
+Set your Magento store to maintenance mode.
+```bash
+bin/magento maintenance:enable
+```
+Install the extension via composer.
+```bash
+composer require taggrs/magento2-hyva-data-layer
+```
+Enable module and perform database upgrade.
+```bash
+bin/magento setup:upgrage
+```
+Perform dependency-injection compilation.
+```bash
+bin/magento setup:di:compile
+```
+Deploy static content.
+```bash
+bin/magento setup:static-content:deploy
+```
+Disable maintenance mode.
+```bash
+bin/magento maintenance:disable
+```
+
+This extension needs no separate configuration from the Taggrs_DataLayer extension. Please refer to the docs of the original extension. 
+
+https://github.com/TAGGRS/TAGGRS_Magento2/blob/main/README.md
